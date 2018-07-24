0db9d99760e9a209c2e92a785a79b6f4e7f59366
David Povey <david.povey@digital.cabinet-office.gov.uk>
2018-07-24 17:08:37 +0100

    Remove unnecessary outputs.
    Solo: @poveyd

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -1,21 +1,7 @@
-output "team_domain_name" {
-  value = "${var.team_name}.${var.top_level_domain_name}"
-}
-
 output "team_zone_id" {
   value = "${aws_route53_zone.primary_zone.zone_id}"
 }
 
 output "team_zone_nameservers" {
   value = "${aws_route53_zone.primary_zone.name_servers}"
-}
-
-resource "null_resource" "Instructions" {
-  depends_on = ["aws_route53_zone.primary_zone"]
-
-  provisioner "local-exec" {
-    command = <<EOT
-echo "\n*\n\nPlease send the following information to whichever team in your organisation looks after the domain name ${var.top_level_domain_name}: \n${var.team_name} = ['${join(".','", aws_route53_zone.primary_zone.name_servers)}.']\n\n*"
-EOT
-  }
 }
